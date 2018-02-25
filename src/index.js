7cb07636725f683b05ea8bae24eb31d57911c996
Polina Kravchuk <Polina Kravchuk>
2018-02-25 19:22:16 +0300

    1-8 (2)

--- src/index.js
+++ src/index.js
@@ -175,7 +175,7 @@ for (var i = arr.length - 1; i >= 0; i--){
 
 					if (arr[j] == '6' && arrSKobka.indexOf(j) == -1) return false;
 
-					if (arr[j] == '8' && arrSKobka.indexOf(j) == -1 && vosem % 2 != 0) return false;
+					if (arr[j] == '8' && arrSKobka.indexOf(j) == -1 && vosem % 2 == 0) return false;
 
 					if (arr[j] == '7' && arrSKobka.indexOf(j) == -1) {
 						arrSKobka.push(i);
@@ -203,7 +203,7 @@ for (var i = arr.length - 1; i >= 0; i--){
 
 					if (arr[j] == '6' && arrSKobka.indexOf(j) == -1) return false;
 
-					if (arr[j] == '7' && arrSKobka.indexOf(j) == -1 && sem % 2 != 0) return false;
+					if (arr[j] == '7' && arrSKobka.indexOf(j) == -1 && sem % 2 == 0) return false;
 
 					if (arr[j] == '8' && arrSKobka.indexOf(j) == -1) {
 						arrSKobka.push(i);
