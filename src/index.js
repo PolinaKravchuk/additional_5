674c0f3fc542e42b0496e540078d2b326c3193da
Polina Kravchuk <Polina Kravchuk>
2018-02-25 17:32:05 +0300

    kruglie skobki

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = 
+function check(str, bracketsConfig) {
+	var mass = [], 
+		opCircul = 0,
+		clCircul = 0,
+		opCirculArr = 0,
+		clCirculArr = 0;
+
+	var arr =  str.split('');
+	console.log('arr ');
+	console.log(arr);
+
+  for (var i = 0; i < bracketsConfig.length; i++){
+  	var row = bracketsConfig[i];
+  	for (var j = 0; j < row.length; j++){
+  		var column = row[j];
+  		mass.push(column);
+  	}
+  }
+  console.log('mass ');
+  console.log(mass);
+	/*for (var i = 0; i < mass.length; i++){
+		if (mass[i] == '(')
+			opCircul ++;
+		if (mass[i] == ')')
+			clCircul ++;
+	}*/
+	var arrSKobka= [];
+	for (var i = arr.length - 1; i >= 0; i--){
+		if (arr[i] == '(') {
+			
+			if (mass.indexOf(arr[i]) != -1){
+				for (var j = i; j < arr.length; j++){
+					if (arr[j] == ')' && mass.indexOf(arr[j]) != -1 && arrSKobka.indexOf(j) == -1) {
+						arrSKobka.push(i);
+						arrSKobka.push(j);
+						break;
+					}
+				}
+			} else console.log('false - net "(" v mass');
+		} 
+	}
+
+	if (arrSKobka.length == arr.length) 
+		console.log('true - vse skobki zapisani');
+	else console.log('false-error');
+	/*var k = 0;
+	if (arr.length % 2 == 0 && opCirculArr == clCirculArr) {
+		for (var i = 0; i < arr.length; i++){
+			var t = arr[i];
+			if (t == '(') {
+				for (var j = i; j < arr.length; j++){
+					if (arr[j] == ')') {
+						k++;
+						break;
+					}
+				}
+				if (k == 1)
+				
+			}
+			
+		}
+	}  else console.log('return false');*/
+
+
+  
 }
